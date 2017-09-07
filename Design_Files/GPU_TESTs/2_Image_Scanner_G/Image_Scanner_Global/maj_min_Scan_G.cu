8d6694d041af03247bbb4dd54f676a182f8f4d56
Hasan Baig <hasan.baig@hotmail.com>
2017-09-07 12:02:33 +0200

    Update maj_min_Scan_G.cu

--- Design_Files/GPU_TESTs/2_Image_Scanner_G/Image_Scanner_Global/maj_min_Scan_G.cu
+++ Design_Files/GPU_TESTs/2_Image_Scanner_G/Image_Scanner_Global/maj_min_Scan_G.cu
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //GPU implementation of Major Minor Axes Scan
 //Author: Hasan Baig
 //Email: hasan.baig@hotmail.com 
@@ -235,5 +235,3 @@ int main (void)
 	return 0;
 }
 
-
->>>>>>> ac3cb0f79f55229281d36df36921a63db1d105b7
