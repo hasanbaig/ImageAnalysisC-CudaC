548b2207bb2ebdd6d4d0a26344879f7288ae5598
Hasan Baig <hasan.baig@hotmail.com>
2017-09-07 12:02:05 +0200

    Update Morph_G_SM.cu

--- Design_Files/GPU_TESTs/1_Noise_Eliminator_G/Morph_G_Shared/Morph_G_SM.cu
+++ Design_Files/GPU_TESTs/1_Noise_Eliminator_G/Morph_G_Shared/Morph_G_SM.cu
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Noise removing through morphological techniques on GPU
 //Author: Hasan Baig
 //Email: hasan.baig@hotmail.com 
@@ -156,5 +156,4 @@ int main (void)
 
 	system ("pause");
 	return 0;
->>>>>>> ac3cb0f79f55229281d36df36921a63db1d105b7
 }
