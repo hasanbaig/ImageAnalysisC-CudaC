f3d79404212f322a9603a5c2c81b28103f0170b8
Hasan Baig <hasan.baig@hotmail.com>
2017-09-07 12:04:01 +0200

    Update Integrated_G.cu

--- Design_Files/GPU_TESTs/Complete_Integration_G/Integrated/Integrated_G.cu
+++ Design_Files/GPU_TESTs/Complete_Integration_G/Integrated/Integrated_G.cu
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //Integrated Code
 //Author: Hasan Baig
 //Email: hasan.baig@hotmail.com 
@@ -369,4 +369,3 @@ int main(void)
 	system("pause");
 	return 0;
 }
->>>>>>> ac3cb0f79f55229281d36df36921a63db1d105b7
